0ade2a94bfa9dd42c491d1133d1e415046236ad8
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2022-03-01 16:37:06 -0300

    Projeto Feign Agenda Telefone Feign Client

---
+++ src/test/java/com/agendatelefonicaone/projetoonefeign2/Projetoonefeign2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendatelefonicaone.projetoonefeign2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetoonefeign2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
